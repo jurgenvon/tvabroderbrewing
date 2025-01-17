ca227d272359cc7220b58e41955e3327a2c6a6cb
Jesper Nyström <jespernystrom90@gmail.com>
2025-01-17 12:54:55 +0100

    testar deploy

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,5 @@
 # Bygg projektet
 ng build --prod --base-href /tvabroderbrewing
 
-# Kopiera CNAME-filen för att återställa custom domain
-echo "www.tbbrewing.com" > dist/tvabroderbrewing/CNAME
-
 # Deploy till GitHub Pages
 ngh --dir=dist/tvabroderbrewing
